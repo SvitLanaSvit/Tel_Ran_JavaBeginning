9336f9099f666c3095468a0867a9e737c5978e56
unknown <svetlichnaya.org@gmail.com>
2022-10-04 19:05:58 +0200

    class.constructor.fields

---
+++ DZ_JavaBeginning_4/src/main/java/game/hero/Elf4.java
@@ -0,0 +1,44 @@
+package game.hero;
+
+public class Elf4 {
+    private int id;
+    private String nickname;
+
+    public Elf4() {}
+
+    public Elf4(int id)
+    {
+        this.id = id;
+    }
+
+    public Elf4(String nickname)
+    {
+        this.nickname = nickname;
+    }
+
+    public Elf4(int id, String nickname)
+    {
+        this.id = id;
+        this.nickname = nickname;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public void setNickname(String nickname)
+    {
+        this.nickname = nickname;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getNickname()
+    {
+        return nickname;
+    }
+}
